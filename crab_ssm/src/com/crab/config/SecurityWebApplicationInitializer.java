57e6b6646138b5ef61c066544d82d33c450fd3b4
0 <0@0-PC>
2015-04-24 10:27:50 +0800

    init content

---
+++ crab_ssm/src/com/crab/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,10 @@
+package com.crab.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer
+{
+//	private static final Logger logger = LogManager.getLogger(SecurityWebApplicationInitializer.class);
+	
+	
+}
